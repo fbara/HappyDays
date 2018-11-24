a377b565184c397f86be3eda78ad11ddc345082f
Frank S. Bara <fbara1@yahoo.com>
2018-11-24 10:40:00 -0600

    Initial Commit

---
+++ HappyDays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyDays
+//
+//  Created by Frank Bara on 11/24/18.
+//  Copyright © 2018 BaraLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
