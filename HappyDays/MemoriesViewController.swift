20f6ed06a396ee94ce154da9ff38f002683de3d7
Frank S. Bara <fbara1@yahoo.com>
2019-06-09 00:00:16 -0500

    Show images in UICollectionView.

--- HappyDays/MemoriesViewController.swift
+++ HappyDays/MemoriesViewController.swift
@@ -11,12 +11,19 @@ import AVFoundation
 import Speech
 import Photos
 
-class MemoriesViewController: UICollectionViewController {
+class MemoriesViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDelegateFlowLayout {
+    
+    var memories = [URL]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         checkPermissions()
+        loadMemories()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+        
     }
     
     // MARK: - Request Permissions
@@ -38,14 +45,172 @@ class MemoriesViewController: UICollectionViewController {
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Helper Functions
+    
+    func getDocumentsDirectory() -> URL {
+        
+        //first, find document directory
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        
+        return documentsDirectory
     }
-    */
 
+    func loadMemories() {
+        //remove existing memories to avoid duplicates.
+        memories.removeAll()
+        
+        //attempt to load all the memories in our documents directory
+        // we need to call with try? because it might fail if there's missing permissions.
+        guard let files = try? FileManager.default.contentsOfDirectory(at: getDocumentsDirectory(), includingPropertiesForKeys: nil, options: []) else { return }
+        
+        //loop over every file found.
+        for file in files {
+            let fileName = file.lastPathComponent
+            
+            // check it ends with ".thumb" so we don't count each memory more than once
+            if fileName.hasSuffix(".thumb") {
+                // get the root name of the memory (i.e., without its path extension)
+                let noExtention = fileName.replacingOccurrences(of: ".thumb", with: "")
+                
+                // create a full path from the memory
+                let memoryPath = getDocumentsDirectory().appendingPathComponent(noExtention)
+                // add it to our array
+                memories.append(memoryPath)
+            }
+        }
+        
+        // reload the list of memories (from second section)
+        collectionView?.reloadSections(IndexSet(integer: 1))
+    }
+    
+    func saveNewMemory(image: UIImage) {
+        // create a unique name for this memory
+        let memoryName = "memory-\(Date().timeIntervalSince1970)"
+        
+        // use the unique name to create filenames for the full-size image and the thumbnail
+        let imageName = memoryName + ".jpg"
+        let thumbnailName = memoryName + ".thumb"
+        
+        do {
+            // create a URL where we can write the JPEG to
+            let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+            
+            // convert the UIImage into a JPEG data object
+            if let jpegData = image.jpegData(compressionQuality: 0.8) {
+                // write that data to the URL we created
+                try jpegData.write(to: imagePath, options: [.atomicWrite])
+            }
+            
+            if let thumbnail = resize(image: image, to: 200) {
+                let imagePath = getDocumentsDirectory().appendingPathComponent(thumbnailName)
+                if let jpegData = thumbnail.jpegData(compressionQuality: 0.8) {
+                    try jpegData.write(to: imagePath, options: [.atomicWrite])
+                }
+            }
+            
+        } catch {
+            print("Failed save to disk")
+        }
+    }
+    
+    func resize(image: UIImage, to width: CGFloat) -> UIImage? {
+        // calculate how much we need to bring the width down to match our target size
+        let scale = width / image.size.width
+        
+        // bring the height down by the same amount so that the aspect ratio is preserved
+        let height = image.size.width * scale
+        
+        // create a new image context we can draw into
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 0)
+        
+        // draw the original image into the context
+        image.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        // pull out the resized version
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // end the context so UIKit can clean up
+        UIGraphicsEndImageContext()
+        
+        // send it back to the caller
+        return newImage
+        
+    }
+    
+    func imageURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("jpg")
+    }
+    
+    func thumbnailURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("thumb")
+    }
+    
+    func audioURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("m4a")
+    }
+    
+    func transcriptionURL(for memory: URL) -> URL {
+        return memory.appendingPathExtension("txt")
+    }
+    
+    
+    // MARK: - Delegate Functions
+    
+    @objc func addTapped() {
+        let vc = UIImagePickerController()
+        vc.modalPresentationStyle = .formSheet
+        vc.delegate = self
+        navigationController?.present(vc, animated: true, completion: nil)
+    }
+    
+    // MARK: - UIImagePicker Functions
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let possibleImage = info[.originalImage] as? UIImage{
+            saveNewMemory(image: possibleImage)
+            loadMemories()
+        }
+    }
+    
+    // MARK: - CollectionView Functions
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 0
+        } else {
+            return memories.count
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // dequeue a cell, pull out the memory it matches, load a UIImage for the thumbnail of that memory, then put it into the cell’s image view
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Memory", for: indexPath) as! MemoryCell
+        
+        let memory = memories[indexPath.row]
+        let imageName = thumbnailURL(for: memory).path
+        let image = UIImage(contentsOfFile: imageName)
+        cell.imageView.image = image
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        // make search bar header work
+        return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Header", for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 1 {
+            return CGSize.zero
+        } else {
+            return CGSize(width: 0, height: 50)
+        }
+    }
 }
